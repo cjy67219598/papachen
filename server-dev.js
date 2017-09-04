654ca13a646284cc22dcefc7a31771ab257ca991
papachen <447334358@qq.com>
2017-09-04 18:53:21 +0800

    update

--- server-dev.js
+++ server-dev.js
@@ -4,8 +4,8 @@ let open = require("open");
 let app = express();
 app.use("/dev",express.static(path.join(__dirname,"dev")));
 app.use("/dist",express.static(path.join(__dirname,"dist")));
-app.listen(23333,() => {
-    console.log("服务开启 @23333");
+app.listen(12345,() => {
+    console.log("服务开启 @12345");
     console.log((new Date()).toLocaleString());
 });
-open("http://localhost:23333/dev/");
+open("http://localhost:12345/dev/");
--- src/components/user/register.vue
+++ src/components/user/register.vue
@@ -9,6 +9,10 @@
                 <Input type="password" v-model="form.password" placeholder="请输入密码">
                 </Input>
             </FormItem>
+            <FormItem prop="rePassword" label="确认密码">
+                <Input type="password" v-model="form.rePassword" placeholder="请再次确认密码">
+                </Input>
+            </FormItem>
             <FormItem prop="nickname" label="昵称">
                 <Input v-model="form.nickname" placeholder="请输入昵称">
                 </Input>
@@ -18,29 +22,31 @@
                 </Input>
             </FormItem>
             <FormItem prop="email" label="邮箱">
-                <Input v-model="form.tel" placeholder="请输入邮箱">
+                <Input v-model="form.email" placeholder="请输入邮箱">
                 </Input>
             </FormItem>
-            <FormItem>
-                <Button type="primary" @click.native="handleSubmit('form')">登录</Button>
-            </FormItem>
+            <div class="text-center">
+                <Button type="primary" @click.native="handleSubmit('form')">提交</Button>
+            </div>
         </Form>
     </div>
 </template>
 <script>
     export default {
         data(){
+            let _this = this;
             return {
                 form:{
                     user:"",
                     password:"",
+                    rePassword:"",
                     nickname:"",
                     email:"",
                     tel:"",
                     introduction:""
                 },
                 rules:{
-                    user:[{
+                    user:[{//用户名验证
                         required:true,
                         message:"请填写用户名",
                         trigger:"blur",
@@ -51,7 +57,7 @@
                             return callback(new Error("用户名必须字母开头，包含字母、数字、下划线，5-15位"));
                         }
                     }],
-                    password:[{
+                    password:[{//密码验证
                         required:true,
                         message:"请填写密码",
                         trigger:"blur"
@@ -61,7 +67,17 @@
                             return callback(new Error("密码只能包含字母、数字、下划线，5-17位"));
                         }
                     }],
-                    nickname:[{
+                    rePassword:[{//重复密码
+                        required:true,
+                        message:"请再次输入密码",
+                        trigger:"blur"
+                    },{
+                        validator(rule, value, callback, source, options){
+                            if(_this.form.password === value) return callback();
+                            return callback(new Error("两次密码输入不一致"));
+                        }
+                    }],
+                    nickname:[{//昵称
                         required:true,
                         message:"请填写昵称",
                         trigger:"blur"
@@ -71,10 +87,16 @@
                             return callback(new Error("昵称最多15字"));
                         }
                     }],
-                    tel:[{
+                    tel:[{//手机号
                         validator(rule, value, callback, source, options){
                             if(/^1[34578]\d{9}$/.test(value) || value === "") return callback();
                             return callback(new Error("请输入正确的手机号"));
+                        }
+                    }],
+                    email:[{//邮箱
+                        validator(rule, value, callback, source, options){
+                            if(/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/.test(value) || value === "") return callback();
+                            return callback(new Error("请输入正确的邮箱"));
                         }
                     }]
                 }
@@ -88,8 +110,11 @@
                     } else {
                         this.$Message.error('表单验证失败!');
                     }
-                })
+                });
             }
+        },
+        mounted(){
+
         }
     }
 </script>
